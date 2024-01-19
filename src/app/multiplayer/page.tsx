94f9adc380d4fe4b05a52f6fd747ee0686d88a57
Lucas Rodrigues <lucasolliveiraa1@gmail.com>
2024-01-19 14:29:38 -0300

    start multiplayer page

--- src/app/multiplayer/page.tsx
+++ src/app/multiplayer/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { MultiplayerContextProvider } from "../lib/context/multiplayer.context/multiplayer.provider";
+import Board from "../ui/components/board";
+import Notation from "../ui/components/notation";
+
 const Page = () => {
   return (
-    <main>
-      <h1>Multiplayer</h1>
+    <main style={{ marginLeft: "21em" }}>
+      <MultiplayerContextProvider>
+        <Board />
+        <Notation />
+      </MultiplayerContextProvider>
     </main>
   );
 };
